848053d845192c4a65ee0ead4d87df19770994fd
Ramya Nagarajan <ramyanaga.99@gmail.com>
2015-11-24 07:11:55 -0800

    added while loop test?

--- ConversionTest/ConverstionTest.java
+++ ConversionTest/ConverstionTest.java
@@ -5,8 +5,10 @@
  * @author (your name) 
  * @version (a version number or a date)
  */
+import java.util.*; 
 public class ConverstionTest {
     public static void main(String[] args) {
+        /*
         int num1 = negativesTest("5"); 
         System.out.println(num1); 
         int num2 = negativesTest("-5");
@@ -15,6 +17,27 @@ public class ConverstionTest {
         System.out.println(num3); 
         int num4 = negativesTest("-1000001"); 
         System.out.println(num4); 
+        */
+       System.out.println("type stuff"); 
+       Scanner sc = new Scanner(System.in); 
+       String number = sc.nextLine(); 
+       int check = 1; 
+       while (check == 1) {
+           //continue = true; 
+           negativesTest("2"); 
+           System.out.println(number); 
+           number = sc.nextLine();
+           System.out.println("number is: " + number); 
+           String numSubstring = number.substring(0,(number.length())); 
+           System.out.println("substring is" + numSubstring); 
+           if (number.substring(0,number.length()).equals("quit")) {
+               System.out.println("in if statement"); 
+               check = 0; 
+               //System.exit(0); 
+               
+            }
+        }
+           
     }
         
    
